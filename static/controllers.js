4f9c8b435d7c13d5ae51920bd82ae47bddc2a374
Koding <pwmvx1289@hotmail.com>
2016-05-08 04:32:48 +0000

    Carousel working

---
+++ static/controllers.js
@@ -0,0 +1,35 @@
+angular.module("howdoibeat", ['ngCookies'])
+
+.config(function($interpolateProvider) {
+  $interpolateProvider.startSymbol('{[');
+  $interpolateProvider.endSymbol(']}');
+})
+
+.run (function ($rootScope, $http, $cookies, $cookieStore, $location){
+ 
+})
+
+.controller("IndexController", function($scope, $rootScope, $http) {
+    $scope.page = {};
+    $scope.page.firstname = 'hi';
+    $rootScope.address = '';
+    $scope.beatdata = {};
+    $http.post ('/leotam1234', {}).
+          success(function(data, status, headers, config) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log(data);
+            console.log (status);
+            //$cookieStore.put ('uid', data.pid);
+            //$cookieStore.put ('oid', data.oid);
+            //window.location.href = $rootScope.url+'dash.html'
+            $scope.beatdata = data;
+            //console.log ($scope.beatdata[0].championId);
+          }).
+          error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log(data);
+            console.log (status);
+          });
+});
